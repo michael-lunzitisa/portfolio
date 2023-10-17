de9b07f0fc8b508a23a2e13485bbbb28eb4c855f
Michael Lunzola <Michael>
2023-10-17 04:36:34 +0100

    modification file app.js

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div className="">
+       <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+
+    </div>
+  );
+}
+ 
+export default App;
