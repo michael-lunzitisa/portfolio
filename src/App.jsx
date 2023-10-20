3497f8d91682e74e6182f4bc421927129457cab9
Michael Lunzola <Michael>
2023-10-20 15:55:41 +0100

    progress bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import NavBar from "./components/NavBar";
 import About from "./components/About";
+import Skils from "./components/Skils";
 
 const App = () => {
   return (
     <div className="h-[100vh] bg-[#313444] w-full">
       <NavBar />
       <About />
+      <Skils />
     </div>
   );
 };
--- src/components/Skils.jsx
+++ src/components/Skils.jsx
@@ -1,5 +1,18 @@
 const Skils = () => {
-  return <div></div>;
+  return (
+    <section className="bg-secondaryColor">
+      <h2 className="text-titleColor text-center text-[45px] font-bold">
+        Skils
+      </h2>
+      <div className="flex justify-center  ">
+        <div className="w-[50%]   bg-titleColor p-0.5 text-center text-white text-xs font-medium">
+          <div className="w-[30%] bg-gray-500 p-0.5 text-center text-white text-xs font-medium">
+            25%
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Skils;
