b03c1a6d3957c40a25d7c9736e0b190c4bb5ea2f
Michael <mickaellunzola@gmail.com>
2023-11-08 08:46:35 +0100

    add boutton submit contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,12 @@
 const Contact = () => {
   return (
-    <div className="bg-secondaryColor">
+    <div className="bg-secondaryColor py-10">
+      <h2 className="text-titleColor text-center text-[45px] font-bold pb-5">
+        Contact
+      </h2>
       <div>
         <form action="submit" method="post">
-          <div class="flex flex-col justify-content-center items-center bg-titleColor">
+          <div class="flex flex-col justify-content-center items-center">
             <input
               type="text"
               name="name"
@@ -19,11 +22,11 @@ const Contact = () => {
             <textarea
               name="message"
               placeholder="Votre message"
-              class="w-[40%]  h-20 px-3 py-2 m-2 border-gray-300 rounded-md"
+              class="w-[40%]  h-40 px-3 py-2 m-2 border-gray-300 rounded-md"
             ></textarea>
             <button
               type="submit"
-              class="w-[15%]  px-3 py-2 m-2 bg-blue-500 text-white rounded-md"
+              class="w-[15%]  px-3 py-2 m-2 bg-titleColor text-white rounded-md"
             >
               Envoyer
             </button>
